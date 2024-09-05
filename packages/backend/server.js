f2979e0bd5b746c38b1c1dc37f65a57a55f70b6b
Sudarshan Adhikari <sudarshan.adhikari@zegal.com>
2024-09-05 13:08:03 +0545

    lerna.json removed from git ignore file

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -8,5 +8,5 @@ app.get('/my-api', (req, res) => {
   res.json({ message: 'Hello start your journey' });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend is running on port ${PORT}`));
